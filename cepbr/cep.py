3bd9c22e7cb8434df1f92ecabd64afa2732bf118
Lucas Magnum <lucas.magnum@pdg.com.br>
2012-12-06 17:46:07 -0200

    Adicionado exceção para casos onde o usuario não informar o CEP.

--- cepbr/cep.py
+++ cepbr/cep.py
@@ -1,10 +1,15 @@
+# -*- coding: utf-8 -*-
+
 #####################################################################
 # CEPBR - Biblioteca consulta ao CEP pelo site dos correios do Brasil
 #
 # author: Mauro Baraldi
 #         Igor Hercowitz
 #
-# v 1.2.0
+# contribuição: Lucas Magnum
+#
+#
+# v 1.2.1
 # usage:
 # >>> tracker = CepTracker()
 # >>> infos = tracker.track(cep)
@@ -13,23 +18,44 @@
 from BeautifulSoup import BeautifulSoup
 import requests
 
+
 class CEP():
-	def __init__(self):
-		self.url = 'http://m.correios.com.br/movel/buscaCepConfirma.do'
-		self.result = []
 
-		self.fields = ['logradouro', 'bairro', ['cidade', 'estado'], 'cep']
+    def __init__(self):
+        self.url = 'http://m.correios.com.br/movel/buscaCepConfirma.do'
+        self.result = []
 
-	def get_cep(self, cep):
-		http_response = requests.post(self.url, 
-			                 data={'cepEntrada': cep, 'tipoCep':'', 'cepTemp':'', 'metodo':'buscarCep'})
+        self.fields = ['logradouro', 'bairro', ['cidade', 'estado'], 'cep']
 
-		if not http_response.status_code == 200:
-			raise requests.HTTPError('Response code %i' % http_response.status_code) 
+    def get_cep(self, cep):
 
-		nodes = BeautifulSoup(http_response.text).findAll('span', {'class':'respostadestaque'})
-		logradouro, bairro, cidade_uf, cep = [n.text.strip() for n in nodes]
-		cidade_uf = [i.strip() for i in cidade_uf.split('\n')]
-		cidade, uf = cidade_uf[0], cidade_uf[-1].replace('/','')
+        if not cep:
+            raise ValueError('É necessário importar um CEP.')
 
-		return {'logradouro':logradouro, 'bairro':bairro, 'cidade':cidade, 'uf':uf, 'cep':cep}
+        http_response = requests.post(
+            self.url,
+            data={
+                'cepEntrada': cep,
+                'tipoCep': '',
+                'cepTemp': '',
+                'metodo': 'buscarCep'
+            })
+
+        if not http_response.status_code == 200:
+            raise requests.HTTPError(
+                'Response code %i' % http_response.status_code)
+
+        nodes = BeautifulSoup(http_response.text).findAll(
+            'span', {'class': 'respostadestaque'})
+
+        logradouro, bairro, cidade_uf, cep = [n.text.strip() for n in nodes]
+        cidade_uf = [i.strip() for i in cidade_uf.split('\n')]
+        cidade, uf = cidade_uf[0], cidade_uf[-1].replace('/', '')
+
+        return {
+            'logradouro': logradouro,
+            'bairro': bairro,
+            'cidade': cidade,
+            'uf': uf,
+            'cep': cep
+        }
